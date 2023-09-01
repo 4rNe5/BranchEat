4fc528333ed438a7ef4836c232ed84f361289ae8
박준현 <me@4rne5.dev>
2023-09-01 13:40:59 +0900

    Initial Commit

---
+++ BranchEat/BranchEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BranchEatApp.swift
+//  BranchEat
+//
+//  Created by 4rNe5 on 2023/09/01.
+//
+
+import SwiftUI
+
+@main
+struct BranchEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
